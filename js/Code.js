6573dd80691181d0b9dcd3839be4de12b4a666ba
zebita <87344216+zebita@users.noreply.github.com>
2021-11-25 12:53:58 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCION";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+   
+    fill("white")
+    textSize(15)
+    text("C U N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: performs a particular task !!",700,170)
+
+    fill("white")
+    textSize(15)
+    text("E D A T E D O S B A S", 100,260)
+    fill("lightblue")
+    text("Hint: Stores all information !!",100,280)
+}
